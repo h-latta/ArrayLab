af2c4352ad15bed3271ce133ff1b073caa0c48da
h-latta <h.latta@yahoo.com>
2022-02-28 14:43:45 -0600

    Completed Problem 6

--- app.js
+++ app.js
@@ -207,15 +207,39 @@ console.log('Even Serving Counts: ', evenServings)
 //5. Create a function that will return dishes whose ingredients array is "tomato" and "cheese".
 //Filter
 
+function problemFive(){
+    let results = dishes.filter(function(el){
+        if(el.ingredients === ["tomato", "cheese"]){
+            return true
+        } else{
+            return false
+        }
+    })
+    return results
+}
 
+let ingredientCheck = problemFive();
+console.log('Food with Tomato and Cheese: ', ingredientCheck)
 
 //6a. Create a function that will return an array of only the names of the cuisine types. Ie ['Italian', 'Mexican', ...]
 //Map
+
+function problemSix(){
+    let result = dishes.map(function(el){
+        return el.cuisine
+    })
+    return result
+}
+
+let cuisineList = problemSix();
+console.log(cuisineList)
+
 // BONUS: (come back to this after finishing all)
 //6b. Use the filter method to eliminate duplicates, leaving only distinct values in the array
 
 //7. Create a function that will append the cuisine type to the start of the dish's name. Ie, "Italian Pizza"
 //Map 
+
 
 //8. Create a function that will append the cuisine type to the start of the dish's name. Then, return only the Vegetarian dish objects. So this function should return objects 11-13 with their names changed to "Vegetarian Lasagna", "Vegetarian Falafel", "Vegetarian Chili"
 //Map, Filter
